b28cfdce66cc6c5867fe62c1b1b36c20323c8510
Fabiola Mayorga <fab.tames@gmail.com>
2016-01-14 16:14:20 -0600

    Adding bower components

---
+++ scripts/game/game_canvas.js
@@ -0,0 +1,16 @@
+angular.module('app.game')
+.directive('gameCanvas', function() {
+	var linkFn = function(scope, ele, attrs){
+		createGame(scope, scope.players, scope.mapId, $injector);
+	};
+
+	return {
+		scope: {
+			players: '=',
+			mapId: '='
+		},
+
+		template: '<div id="gameCanvas"></div>',
+		link: linkFn
+	}
+});
---
+++ scripts/game/main.js
@@ -0,0 +1,5 @@
+window.createGame = function(scope, players, mapId, injector){
+	var height = parseInt(ele.css('height'), 10),
+      width   = parseInt(ele.css('width'), 10);
+  	var game = new Phaser.Game(width, height, Phaser.AUTO, 'gameCanvas');	
+}
